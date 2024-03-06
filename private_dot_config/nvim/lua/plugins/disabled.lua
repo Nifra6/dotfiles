9c2f0626bf13cd699be5a21c2c6dadd19bd3fdcd
Nicolas MAIGNAN <nicolas.maignan@univ-lorraine.fr>
2024-03-06 17:46:22 +0100

    add nvim powered by lazy.nvim

---
+++ private_dot_config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,60 @@
+return {
+    -- Coding
+    { "nvim-cmp", enabled = false },
+    { "cmp-nvim-lsp", enabled = false },
+    { "cmp-buffer", enabled = false },
+    { "cmp-path", enabled = false },
+    { "LuaSnip", enabled = false },
+    { "friendly-snippets", enabled = false },
+    { "cmp_luasnip", enabled = false },
+    { "mini.pairs", enabled = false },
+    { "mini.surround", enabled = false },
+    { "nvim-ts-context-commentstring", enabled = false },
+    { "mini.comment", enabled = false },
+    { "mini.ai", enabled = false },
+    -- Colorscheme
+    --{ "tokyonight.nvim", enabled = false }, -- colorscheme
+    { "catppucin", enabled = false },  -- another colorscheme
+    -- Editor
+    --{ "neo-tree.nvim", enabled = false },  -- file explorer on the left
+    --{ "nvim-spectre", enabled = false },   --search/replace in all files
+    { "telescope-fzf-native.nvim", enabled = false },
+    { "flash.nvim", enabled = false },
+    { "gitsigns.nvim", enabled = false },
+    { "vim-illuminate", enabled = false },
+    { "trouble.nvim", enabled = false },
+    { "todo-comments.nvim", enabled = false },
+    --{ "telescope.nvim", enabled = false },  -- fuzzy finder
+    -- Formatting
+    { "conform.nvim", enabled = false },
+    -- Linting
+    { "nvim-lint", enabled = false },
+    -- LSP
+    { "nvim-lspconfig", enabled = false },
+    { "neoconf.nvim", enabled = false },
+    { "neodev.nvim", enabled = false },
+    { "mason.nvim", enabled = false },
+    { "mason-lspconfig.nvim", enabled = false },
+    -- TreeSitter
+    { "nvim-treesiter", enabled = false },
+    { "nvim-treesiter-textobjects", enabled = false },
+    { "nvim-treesiter-context", enabled = false },
+    { "nvim-ts-autotag", enabled = false },
+    -- UI
+    --{ "nvim-notify", enabled = false }, -- Notifications at top right corner
+    { "dressing.nvim", enabled = false }, -- ?
+    --{ "bufferline.nvim", enabled = false }, -- tabs for multiple files
+    --{ "mini.bufremove", enabled = false },  -- close tab
+    --{ "lualine.nvim", enabled = false }, --statusline
+    --{ "indent-blankline.nvim", enabled = false }, -- show all indents
+    --{ "mini.indentscope", enabled = false }, -- highlight current indent
+    --{ "which-key.nvim", enabled = false }, -- show otpion when key is pressed
+    { "noice.nvim", enabled = false },-- ?
+    --{ "nvim-web-devicons", enabled = false }, -- icons
+    --{ "nui.nvim", enabled = false },    --lib
+    --{ "dashboard-nvim", enabled = false }, -- Welcome page
+    -- Util
+    { "vim-startuptime", enabled = false },   -- count startuptime, not very useful
+    --{ "persistence.nvim", enabled = false }, -- load previous sessions
+    --{ "plenary.nvim", enabled = false },   -- lib
+}
