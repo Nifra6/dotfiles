8d031da122461900f6a89097fe7a6138165a27ac
Nicolas MAIGNAN <nicolas.maignan@univ-lorraine.fr>
2024-03-12 14:52:40 +0100

    clean lazyvim

--- private_dot_config/nvim/lua/plugins/editor.lua
+++
@@ -1,5 +0,0 @@
-return {
-    "neo-tree.nvim",    -- file explorer on the left
-    "nvim-spectre",     --search/replace in all files
-    "telescope.nvim",   -- fuzzy finder
-}
--- private_dot_config/nvim/lua/plugins/util.lua
+++
@@ -1,7 +0,0 @@
-return {
-    -- Loading previous sessions
-    "folke/persistence.nvim", 
-    -- Lib
-    "MunifTanjim/nui.nvim",
-    "nvim-lua/plenary.nvim",
-}
