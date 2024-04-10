dfeb18006b7491df0be3445b55e928eccc2c6787
Nicolas MAIGNAN <nicolas.maignan@univ-lorraine.fr>
2024-04-10 09:53:42 +0200

    add numlock on regolith + workspace assignement + lsd have issue on ubuntu

---
+++ private_dot_config/regolith3/i3/config.d
@@ -0,0 +1,9 @@
+# Enable NumLock at startup
+exec --no-startup-id numlockx on
+
+# Assign applications to specific workspaces
+assign [class="discord"] $workspace1
+assign [class="Spotify"] $workspace2
+assign [class="Code"] $workspace5
+assign [class="Thunderbird"] $workspace6
+assign [class="Zotero"] $workspace8
