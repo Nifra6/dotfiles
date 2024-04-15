864cd01331fd47320b1364ac945c71db865aa34a
Nicolas MAIGNAN <nicolas.maignan@univ-lorraine.fr>
2024-04-15 11:12:04 +0200

    add media and weather modules on i3 bar

--- private_dot_config/regolith3/i3/config.d
+++ private_dot_config/regolith3/i3/config.d
@@ -1,9 +1,2 @@
 # Enable NumLock at startup
 exec --no-startup-id numlockx on
-
-# Assign applications to specific workspaces
-assign [class="discord"] $workspace1
-assign [class="spotify"] $workspace2
-assign [class="code"] $workspace5
-assign [class="thunderbird"] $workspace6
-assign [class="Zotero"] $workspace8
