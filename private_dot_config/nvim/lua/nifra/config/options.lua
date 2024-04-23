9eea032f20777a569f984a13cfed5261e1ba9424
Nicolas MAIGNAN <nicolas.maignan@univ-lorraine.fr>
2024-04-23 21:02:41 +0200

    start my profile on nvim

---
+++ private_dot_config/nvim/lua/nifra/config/options.lua
@@ -0,0 +1,16 @@
+local opt = vim.opt
+
+-- Line numbers
+opt.number = true
+opt.relativenumber = true
+
+-- Enable mouse
+opt.mouse = "a"
+
+-- Tabs behavior for indent
+opt.expandtab = true
+opt.shiftwidth = 4
+
+-- Auto-indent
+opt.smartindent = true
+opt.autoindent = true
