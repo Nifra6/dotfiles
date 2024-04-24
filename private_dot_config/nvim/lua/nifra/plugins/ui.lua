903f22f36e8fb4053ed439d4ca20227160315516
Nicolas MAIGNAN <nicolas.maignan@univ-lorraine.fr>
2024-04-24 16:54:22 +0200

    activate bufferline + move plugins

--- private_dot_config/nvim/lua/nifra/plugins/ui.lua
+++ private_dot_config/nvim/lua/nifra/plugins/ui.lua
@@ -1,11 +1,6 @@
 return {
     -- NOTE: Colorscheme
-    {
-        "folke/tokyonight.nvim",
-        lazy = false,
-        priority = 1000,
-        opts = {},
-    },
+    { "folke/tokyonight.nvim", lazy = false, priority = 1000 },
     -- NOTE: Welcome page
     {
         "nvimdev/dashboard-nvim",
@@ -33,7 +28,7 @@ return {
                 },
             })
         end,
-        dependencies = { { "nvim-tree/nvim-web-devicons" } },
+        dependencies = { "nvim-tree/nvim-web-devicons" },
     },
     -- NOTE: Notifications and command line input
     {
@@ -51,4 +46,23 @@ return {
         dependencies = { "nvim-tree/nvim-web-devicons" },
         opts = { options = { theme = "tokyonight" } },
     },
+    -- NOTE: File explorer
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        -- config = function()
+        --     require("neo-tree").setup()
+        -- end,
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim",
+            -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+        },
+        keys = {
+            { "<leader>tt", "<Cmd>Neotree toggle<CR>", desc = "Neo[T]ree" },
+        },
+    },
+    -- NOTE: Buffer tabs
+    { "akinsho/bufferline.nvim", version = "*", config = true, dependencies = "nvim-tree/nvim-web-devicons" },
 }
