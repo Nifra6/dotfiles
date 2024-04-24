a9032af3a682be0559a68d8afe17ee44587b7958
Nicolas MAIGNAN <nicolas.maignan@univ-lorraine.fr>
2024-04-24 15:03:25 +0200

    extract vim config and lazy boostrap from main init file

--- private_dot_config/nvim/lua/nifra/config/options.lua
+++ private_dot_config/nvim/lua/nifra/config/options.lua
@@ -1,16 +1,51 @@
 local opt = vim.opt
 
--- Line numbers
+-- Lines
 opt.number = true
 opt.relativenumber = true
+opt.cursorline = true
+opt.scrolloff = 10
 
--- Enable mouse
+-- Search
+opt.ignorecase = true
+opt.smartcase = true
+opt.hlsearch = true
+
+-- Mouse
 opt.mouse = "a"
 
--- Tabs behavior for indent
+-- Clipboard
+opt.clipboard = "unnamedplus"
+
+-- Indent
 opt.expandtab = true
 opt.shiftwidth = 4
-
--- Auto-indent
 opt.smartindent = true
 opt.autoindent = true
+opt.breakindent = true
+
+-- Save undo history
+opt.undofile = true
+
+-- Keep signcolumn on by default
+vim.opt.signcolumn = "yes"
+
+-- Decrease update time
+vim.opt.updatetime = 250
+
+-- Decrease mapped sequence wait time
+-- Displays which-key popup sooner
+vim.opt.timeoutlen = 300
+
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = "split"
