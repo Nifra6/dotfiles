6321c36d42575e8c785b8a3c3769fcbe775cdf98
Nicolas MAIGNAN <nicolas.maignan@univ-lorraine.fr>
2024-04-29 16:11:44 +0200

    better keys for autocomplete ?

--- private_dot_config/nvim/lua/nifra/plugins/editor.lua
+++ private_dot_config/nvim/lua/nifra/plugins/editor.lua
@@ -26,6 +26,7 @@ return {
         dependencies = { "nvim-lua/plenary.nvim" },
         opts = { signs = true },
     },
+
     -- NOTE: Git indications
     {
         "lewis6991/gitsigns.nvim",
@@ -124,32 +125,13 @@ return {
                 --
                 -- No, but seriously. Please read `:help ins-completion`, it is really good!
                 mapping = cmp.mapping.preset.insert({
-                    -- Select the [n]ext item
-                    ["<C-n>"] = cmp.mapping.select_next_item(),
-                    -- Select the [p]revious item
-                    ["<C-p>"] = cmp.mapping.select_prev_item(),
-
-                    -- Scroll the documentation window [b]ack / [f]orward
-                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-
-                    -- Accept ([y]es) the completion.
-                    --  This will auto-import if your LSP supports it.
-                    --  This will expand snippets if the LSP sent a snippet.
-                    ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-
-                    -- If you prefer more traditional completion keymaps,
-                    -- you can uncomment the following lines
-                    ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+                    ["²"] = cmp.mapping.confirm({ select = true }),
+                    ["<Enter>"] = cmp.mapping.confirm({ select = true }),
+                    ["<Tab>"] = cmp.mapping.select_next_item(),
                     ["<Down>"] = cmp.mapping.select_next_item(),
                     ["<Up>"] = cmp.mapping.select_prev_item(),
-                    ["²"] = cmp.mapping.select_next_item(),
                     ["<Esc>"] = cmp.mapping.abort(),
-
-                    -- Manually trigger a completion from nvim-cmp.
-                    --  Generally you don't need this, because nvim-cmp will display
-                    --  completions whenever it has completion options available.
-                    ["<C-Space>"] = cmp.mapping.complete({}),
+                    ["<C-Backspace>"] = cmp.mapping.abort(),
 
                     -- Think of <c-l> as moving to the right of your snippet expansion.
                     --  So if you have a snippet that's like:
