8c5757dc28c5c981914709e0b2dfec1f81b79427
Nicolas MAIGNAN <nicolas.maignan@univ-lorraine.fr>
2024-05-03 17:10:44 +0200

    review telescope config

--- private_dot_config/nvim/lua/nifra/plugins/finder.lua
+++ private_dot_config/nvim/lua/nifra/plugins/finder.lua
@@ -1,5 +1,5 @@
 return {
-    -- FIXME: Fuzzy finder
+    -- NOTE: Fuzzy finder
     {
         "nvim-telescope/telescope.nvim",
         event = "VeryLazy",
@@ -14,21 +14,11 @@ return {
                 end,
             },
             "nvim-telescope/telescope-ui-select.nvim",
-            { "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
+            "nvim-tree/nvim-web-devicons",
         },
         config = function()
-            -- [[ Configure Telescope ]]
-            -- See `:help telescope` and `:help telescope.setup()`
+            -- TODO: Check `:help telescope.setup()`
             require("telescope").setup({
-                -- You can put your default mappings / updates / etc. in here
-                --  All the info you're looking for is in `:help telescope.setup()`
-                --
-                -- defaults = {
-                --   mappings = {
-                --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-                --   },
-                -- },
-                -- pickers = {}
                 extensions = {
                     ["ui-select"] = {
                         require("telescope.themes").get_dropdown(),
@@ -36,27 +26,19 @@ return {
                 },
             })
 
-            -- Enable Telescope extensions if they are installed
+            --NOTE: Enable Telescope extensions
             pcall(require("telescope").load_extension, "fzf")
             pcall(require("telescope").load_extension, "ui-select")
 
-            -- See `:help telescope.builtin`
+            --NOTE: Keymaps
             local builtin = require("telescope.builtin")
             vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[H]elp" })
             vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "[K]eymaps" })
             vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[F]iles" })
-            -- vim.keymap.set("n", "<leader>fs", builtin.builtin, { desc = "[S]elect Telescope" })
             vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "Current [W]ord" })
             vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[G]rep" })
             vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "[D]iagnostics" })
-            vim.keymap.set("n", "<leader>fr", builtin.resume, { desc = "[R]esume" })
-            vim.keymap.set("n", "<leader>f.", builtin.oldfiles, { desc = 'Recent Files ("." for repeat)' })
             vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "Find existing buffers" })
-
-            -- Shortcut for searching your Neovim configuration files
-            -- vim.keymap.set("n", "<leader>sn", function()
-            --     builtin.find_files({ cwd = vim.fn.stdpath("config") })
-            -- end, { desc = "[N]eovim files" })
         end,
     },
 }
