b8ef4b9835e50dacd6659109d173530fb5cb54b0
Nicolas MAIGNAN <nicolas.maignan@univ-lorraine.fr>
2024-05-03 11:44:17 +0200

    configured nvim-cmp

--- private_dot_config/nvim/lua/nifra/plugins/completion.lua
+++ private_dot_config/nvim/lua/nifra/plugins/completion.lua
@@ -1,42 +1,30 @@
 return {
-    -- FIXME: Auto completion
+    -- NOTE: Auto completion
     {
         "hrsh7th/nvim-cmp",
         event = "InsertEnter",
         dependencies = {
             {
                 "L3MON4D3/LuaSnip",
-                build = (function()
-                    -- Build Step is needed for regex support in snippets.
-                    -- This step is not supported in many windows environments.
-                    -- Remove the below condition to re-enable on windows.
+                build = function()
                     if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
                         return
                     end
                     return "make install_jsregexp"
-                end)(),
+                end,
+                -- TODO: Explore in details what is available
                 dependencies = {
-                    -- `friendly-snippets` contains a variety of premade snippets.
-                    --    See the README about individual language/framework/plugin snippets:
-                    --    https://github.com/rafamadriz/friendly-snippets
-                    -- {
-                    --   'rafamadriz/friendly-snippets',
-                    --   config = function()
-                    --     require('luasnip.loaders.from_vscode').lazy_load()
-                    --   end,
-                    -- },
+                    "rafamadriz/friendly-snippets",
+                    config = function()
+                        require("luasnip.loaders.from_vscode").lazy_load()
+                    end,
                 },
             },
             "saadparwaiz1/cmp_luasnip",
-
-            -- Adds other completion capabilities.
-            --  nvim-cmp does not ship with all sources by default. They are split
-            --  into multiple repos for maintenance purposes.
             "hrsh7th/cmp-nvim-lsp",
             "hrsh7th/cmp-path",
         },
         config = function()
-            -- See `:help cmp`
             local cmp = require("cmp")
             local luasnip = require("luasnip")
             luasnip.config.setup({})
@@ -48,26 +36,22 @@ return {
                     end,
                 },
                 completion = { completeopt = "menu,menuone,noinsert" },
-
-                -- For an understanding of why these mappings were
-                -- chosen, you will need to read `:help ins-completion`
-                --
-                -- No, but seriously. Please read `:help ins-completion`, it is really good!
+                -- NOTE: Keymaps
                 mapping = cmp.mapping.preset.insert({
+                    -- NOTE: Accept completion
                     ["²"] = cmp.mapping.confirm({ select = true }),
                     ["<Enter>"] = cmp.mapping.confirm({ select = true }),
+                    -- NOTE: Scroll completion
                     ["<Tab>"] = cmp.mapping.select_next_item(),
                     ["<Down>"] = cmp.mapping.select_next_item(),
                     ["<Up>"] = cmp.mapping.select_prev_item(),
-                    -- ["<Esc>"] = cmp.mapping.abort(),
+                    -- NOTE: Abort completion
                     ["<C-Backspace>"] = cmp.mapping.abort(),
-
-                    -- Think of <c-l> as moving to the right of your snippet expansion.
-                    --  So if you have a snippet that's like:
+                    -- NOTE: Navigate snippet
+                    --  If you have a snippet that's like:
                     --  function $name($args)
                     --    $body
                     --  end
-                    --
                     -- <c-l> will move you to the right of each of the expansion locations.
                     -- <c-h> is similar, except moving you backwards.
                     ["<C-l>"] = cmp.mapping(function()
@@ -80,14 +64,12 @@ return {
                             luasnip.jump(-1)
                         end
                     end, { "i", "s" }),
-
-                    -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-                    --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
                 }),
+                -- NOTE: Sources of information for cmp (need the plugins as dependencies)
                 sources = {
-                    { name = "nvim_lsp" },
-                    { name = "luasnip" },
-                    { name = "path" },
+                    { name = "nvim_lsp" }, -- LSP
+                    { name = "luasnip" }, -- Snippets
+                    { name = "path" }, -- Path
                 },
             })
         end,
