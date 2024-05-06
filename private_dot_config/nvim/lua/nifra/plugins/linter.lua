27cceec91c764752e588cb1f6d487c9da96e0fc8
Nicolas MAIGNAN <nicolas.maignan@univ-lorraine.fr>
2024-05-06 14:47:25 +0200

    linter configuration

--- private_dot_config/nvim/lua/nifra/plugins/linter.lua
+++ private_dot_config/nvim/lua/nifra/plugins/linter.lua
@@ -1,29 +1,25 @@
 return {
-    -- FIXME: Lint code
+    -- NOTE: Lint code
     {
         "mfussenegger/nvim-lint",
         event = { "BufNewFile", "BufReadPost" },
         opts = {
             events = { "BufWritePost", "BufReadPost", "InsertLeave" },
             linters_by_ft = {
-                -- ["*"] = { "typos" },
                 bash = { "shellcheck" },
                 markdown = { "markdownlint" },
-                python = { "ruff" },
+                python = { "pylint" },
                 sh = { "shellcheck" },
-                -- Use the "*" filetype to run linters on all filetypes.
-                -- ['*'] = { 'global linter' },
-                -- Use the "_" filetype to run linters on filetypes that don't have other linters configured.
-                -- ['_'] = { 'fallback linter' },
+                -- ["*"] = { "typos" }, -- NOTE: Global linter
+                -- ["_"] = {}, -- NOTE: Fallback linter
             },
-            -- LazyVim extension to easily override linter options
-            -- or add custom linters.
-            ---@type table<string,table>
+            ---@type table<string,table>  NOTE: Override linter options
             linters = {},
         },
         config = function(_, opts)
             local M = {}
 
+            -- NOTE: Selects linters by file types
             local lint = require("lint")
             for name, linter in pairs(opts.linters) do
                 if type(linter) == "table" and type(lint.linters[name]) == "table" then
@@ -34,6 +30,7 @@ return {
             end
             lint.linters_by_ft = opts.linters_by_ft
 
+            -- NOTE: Delays function calls
             function M.debounce(ms, fn)
                 local timer = vim.uv.new_timer()
                 return function(...)
@@ -45,25 +42,20 @@ return {
                 end
             end
 
+            -- NOTE: Run linters
             function M.lint()
-                -- Use nvim-lint's logic first:
-                -- * checks if linters exist for the full filetype first
-                -- * otherwise will split filetype by "." and add all those linters
-                -- * this differs from conform.nvim which only uses the first filetype that has a formatter
                 local names = lint._resolve_linter_by_ft(vim.bo.filetype)
-
-                -- Create a copy of the names table to avoid modifying the original.
                 names = vim.list_extend({}, names)
 
-                -- Add fallback linters.
+                -- Add fallback linters
                 if #names == 0 then
                     vim.list_extend(names, lint.linters_by_ft["_"] or {})
                 end
 
-                -- Add global linters.
+                -- Add global linters
                 vim.list_extend(names, lint.linters_by_ft["*"] or {})
 
-                -- Filter out linters that don't exist or don't match the condition.
+                -- Filter out linters that don't exist or don't match the condition
                 local ctx = { filename = vim.api.nvim_buf_get_name(0) }
                 ctx.dirname = vim.fn.fnamemodify(ctx.filename, ":h")
                 names = vim.tbl_filter(function(name)
@@ -74,12 +66,13 @@ return {
                     return linter and not (type(linter) == "table" and linter.condition and not linter.condition(ctx))
                 end, names)
 
-                -- Run linters.
+                -- Run linters
                 if #names > 0 then
                     lint.try_lint(names)
                 end
             end
 
+            -- NOTE: Autocommand to launch linting
             vim.api.nvim_create_autocmd(opts.events, {
                 group = vim.api.nvim_create_augroup("nvim-lint", { clear = true }),
                 callback = M.debounce(100, M.lint),
