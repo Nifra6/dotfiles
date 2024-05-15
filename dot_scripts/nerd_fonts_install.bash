3f365aba5e8011984a3181908e69aedcf20a1477
Nicolas MAIGNAN <nicolas.maignan@univ-lorraine.fr>
2024-05-15 16:51:57 +0200

    update nerd fonts version

--- dot_scripts/nerd_fonts_install.bash
+++ dot_scripts/nerd_fonts_install.bash
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 declare -a fonts=(
-	JetBrainsMono
-        Ubuntu
+    JetBrainsMono
+    Ubuntu
 )
 
-version='2.2.2'
+version='3.2.1'
 fonts_dir="${HOME}/.local/share/fonts"
 
 if [[ ! -d "$fonts_dir" ]]; then
-	mkdir -p "$fonts_dir"
+    mkdir -p "$fonts_dir"
 fi
 
 for font in "${fonts[@]}"; do
-	zip_file="${font}.zip"
-	download_url="https://github.com/ryanoasis/nerd-fonts/releases/download/v${version}/${zip_file}"
-	echo "Downloading $download_url"
-	wget "$download_url"
-	unzip "$zip_file" -d "$fonts_dir" -x "*.txt/*" -x "*.md/*"
-	rm "$zip_file"
+    zip_file="${font}.zip"
+    download_url="https://github.com/ryanoasis/nerd-fonts/releases/download/v${version}/${zip_file}"
+    echo "Downloading $download_url"
+    wget "$download_url"
+    unzip "$zip_file" -d "$fonts_dir" -x "*.txt/*" -x "*.md/*"
+    rm "$zip_file"
 done
 
 find "$fonts_dir" -name '*Windows Compatible*' -delete
