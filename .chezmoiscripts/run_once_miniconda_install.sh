00356a1eda3fe51389f5db39b2ab7a38070b406b
Nicolas MAIGNAN <nicolas.maignan@univ-lorraine.fr>
2024-12-20 16:40:41 +0100

    Miniconda autoinstall

---
+++ .chezmoiscripts/run_once_miniconda_install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+MINICONDA_DIR="$HOME/Applications/miniconda3"
+
+if [ ! -d "$MINICONDA_DIR" ]; then
+    mkdir -p "$MINICONDA_DIR"
+    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O "$MINICONDA_DIR"/miniconda.sh
+    zsh "$MINICONDA_DIR"/miniconda.sh -b -u -p "$MINICONDA_DIR"
+    rm "$MINICONDA_DIR"/miniconda.sh
+fi
