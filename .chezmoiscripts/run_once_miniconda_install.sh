a5a164f60fb862b98fe69dd038df57f088f9eec9
Nicolas MAIGNAN <nicolas.maignan@univ-lorraine.fr>
2024-12-20 16:45:20 +0100

    Little fix

--- .chezmoiscripts/run_once_miniconda_install.sh
+++ .chezmoiscripts/run_once_miniconda_install.sh
@@ -7,4 +7,5 @@ if [ ! -d "$MINICONDA_DIR" ]; then
     wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O "$MINICONDA_DIR"/miniconda.sh
     zsh "$MINICONDA_DIR"/miniconda.sh -b -u -p "$MINICONDA_DIR"
     rm "$MINICONDA_DIR"/miniconda.sh
+    echo "Dotfiles: Miniconda installed at $MINICONDA_DIR"
 fi
