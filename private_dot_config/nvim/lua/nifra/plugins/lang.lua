f6f5c9e566f7f92d5910436385e75fd3ec36ff5a
Nicolas MAIGNAN <nicolas.maignan@univ-lorraine.fr>
2025-04-24 18:07:30 +0200

    Simplify things

--- private_dot_config/nvim/lua/nifra/plugins/lang.lua
+++ private_dot_config/nvim/lua/nifra/plugins/lang.lua
@@ -46,10 +46,6 @@ return {
     {
         "OXY2DEV/markview.nvim",
         lazy = false,
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter",
-            "nvim-tree/nvim-web-devicons",
-        },
     },
     -- NOTE: Python : select virtual environment
     {
--- private_dot_config/nvim/lua/nifra/plugins/ui.lua
+++ private_dot_config/nvim/lua/nifra/plugins/ui.lua
@@ -13,9 +13,6 @@ return {
     {
         "OXY2DEV/helpview.nvim",
         lazy = false,
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter",
-        },
     },
     -- NOTE: Welcome page
     {
