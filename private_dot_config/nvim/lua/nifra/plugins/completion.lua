87f7c66f2f6a750603ebb9c688811b1aa8778e5a
Nicolas MAIGNAN <nicolas.maignan@univ-lorraine.fr>
2025-06-30 11:08:58 +0200

    Better blink.cmp config

--- private_dot_config/nvim/lua/nifra/plugins/completion.lua
+++ private_dot_config/nvim/lua/nifra/plugins/completion.lua
@@ -1,19 +1,48 @@
+---@diagnostic disable: undefined-doc-name
+
 return {
+    -- NOTE: Completions for neovim config editing
+    {
+        "folke/lazydev.nvim",
+        ft = "lua", -- only load on lua files
+        opts = {
+            library = {
+                -- See the configuration section for more details
+                -- Load luvit types when the `vim.uv` word is found
+                { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+            },
+        },
+    },
+    -- NOTE: Completions engine
     {
         'saghen/blink.cmp',
         dependencies = { 'rafamadriz/friendly-snippets' },
         version = '1.*',
+
+        ---@module 'blink.cmp'
+        ---@type blink.cmp.Config
         opts = {
             keymap = {
                 preset = 'default',
-                ["<Enter>"] = { "accept" },
-                ["<Tab>"] = { "select_next" },
+                ["<Enter>"] = { "accept", "fallback" },
+                ["<Tab>"] = { "select_next", "fallback" },
             },
             appearance = { nerd_font_variant = 'mono' },
-            completion = { documentation = { auto_show = true } },
-            sources = {
-                default = { 'lsp', 'path', 'snippets', 'buffer' },
+            completion = {
+                documentation = { auto_show = false },
             },
+            sources = {
+                default = { "lazydev", 'lsp', 'path', 'snippets', 'buffer' },
+                providers = {
+                    lazydev = {
+                        name = "LazyDev",
+                        module = "lazydev.integrations.blink",
+                        -- make lazydev completions top priority (see `:h blink.cmp`)
+                        score_offset = 100,
+                    },
+                },
+            },
+
             -- See the fuzzy documentation for more information
             fuzzy = { implementation = "prefer_rust_with_warning" }
         },
