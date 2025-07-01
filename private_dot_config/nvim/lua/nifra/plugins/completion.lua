3afcf57c4169a298a07571b5960bd0233c2f749e
Nicolas MAIGNAN <nicolas.maignan@univ-lorraine.fr>
2025-07-01 18:32:24 +0200

    Move personnal snippets in a named sub folder

--- private_dot_config/nvim/lua/nifra/plugins/completion.lua
+++ private_dot_config/nvim/lua/nifra/plugins/completion.lua
@@ -36,13 +36,10 @@ return {
                     lazydev = {
                         name = "LazyDev",
                         module = "lazydev.integrations.blink",
-                        -- make lazydev completions top priority (see `:h blink.cmp`)
                         score_offset = 100,
                     },
                 },
             },
-
-            -- See the fuzzy documentation for more information
             fuzzy = { implementation = "prefer_rust_with_warning" },
         },
         opts_extend = { "sources.default" },
