fd9891c9f99d1d92804ab27972894fb40c852720
Nicolas MAIGNAN <nicolas.maignan@univ-lorraine.fr>
2025-07-02 18:47:31 +0200

    Better lazy.nvim config

--- private_dot_config/nvim/lua/nifra/lazy.lua
+++ private_dot_config/nvim/lua/nifra/lazy.lua
@@ -1,16 +1,25 @@
 -- NOTE: Clone lazy.nvim if it's not already present
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable",
-        lazypath,
-    })
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 
 -- NOTE: Setup lazy.nvim
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup("nifra.plugins", { checker = { enabled = true, notify = false } })
+require("lazy").setup({
+    spec = {
+        { import = "nifra.plugins" },
+    },
+    install = { colorscheme = { "tokyonight-moon" } },
+    checker = { enabled = true, notify = false },
+})
