7cc7b1fed0b6f0f1e69997c1679e2596d174deae
Nicolas MAIGNAN <nicolas.maignan@univ-lorraine.fr>
2026-02-09 15:40:36 +0100

    Add build/ dir for LaTeX compilation

--- private_dot_config/nvim/lua/nifra/plugins/lang.lua
+++ private_dot_config/nvim/lua/nifra/plugins/lang.lua
@@ -6,6 +6,7 @@ return {
         init = function()
             vim.g.vimtex_view_method = "zathura"
             vim.g.vimtex_compiler_method = "latexmk"
+            vim.g.vimtex_compiler_latexmk = { out_dir = "build" }
             vim.g.vimtex_format_enabled = 1
         end,
         keys = {
