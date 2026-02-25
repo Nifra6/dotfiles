3516d24192d0e9241d1621c943bfe089251addf4
Nicolas MAIGNAN <nicolas.maignan@univ-lorraine.fr>
2026-02-25 14:19:52 +0100

    Prepare update of treesitter

--- private_dot_config/nvim/lua/nifra/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/nifra/plugins/treesitter.lua
@@ -1,45 +1,52 @@
-return {
-    -- NOTE: Highlight, edit, and navigate code
-    {
-        "nvim-treesitter/nvim-treesitter",
-        lazy = false,
-        build = ":TSUpdate",
-        branch = "master",
-        dependencies = { "OXY2DEV/markview.nvim" },
-        opts = {
-            ensure_installed = {
-                "bash",
-                "css",
-                "dockerfile",
-                "git_config",
-                "gitignore",
-                "hyprlang",
-                "java",
-                "lua",
-                "luadoc",
-                "markdown",
-                "markdown_inline",
-                "matlab",
-                "python",
-                "requirements",
-                "ssh_config",
-                "toml",
-                "typst",
-                "vim",
-                "vimdoc",
-                "yaml",
-                "zathurarc",
-                "zig",
-            },
-            auto_install = false,
-            highlight = {
-                enable = true,
-            },
-            -- indent = { enable = true },
-        },
-        config = function(_, opts)
-            require("nvim-treesitter.install").prefer_git = true
-            require("nvim-treesitter.configs").setup(opts)
-        end,
-    },
+-- NOTE: Highlight, edit, and navigate code
+
+local treesitter = {
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    build = ":TSUpdate",
 }
+
+local master_treesitter = {
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    build = ":TSUpdate",
+    branch = "master",
+    dependencies = { "OXY2DEV/markview.nvim" },
+    opts = {
+        ensure_installed = {
+            "bash",
+            "css",
+            "dockerfile",
+            "git_config",
+            "gitignore",
+            "hyprlang",
+            "java",
+            "lua",
+            "luadoc",
+            "markdown",
+            "markdown_inline",
+            "matlab",
+            "python",
+            "requirements",
+            "ssh_config",
+            "toml",
+            "typst",
+            "vim",
+            "vimdoc",
+            "yaml",
+            "zathurarc",
+            "zig",
+        },
+        auto_install = false,
+        highlight = {
+            enable = true,
+        },
+        -- indent = { enable = true },
+    },
+    config = function(_, opts)
+        require("nvim-treesitter.install").prefer_git = true
+        require("nvim-treesitter.configs").setup(opts)
+    end,
+}
+
+return { master_treesitter }
