8a0da8c23e18220d4c71d0990647d53f7f1c808b
Nicolas MAIGNAN <nicolas.maignan@univ-lorraine.fr>
2026-02-25 15:17:00 +0100

    Update LSPs root markers

--- private_dot_config/nvim/lsp/gitlab_ci_ls.lua
+++ private_dot_config/nvim/lsp/gitlab_ci_ls.lua
@@ -3,7 +3,7 @@ local cache_dir = vim.uv.os_homedir() .. "/.cache/gitlab-ci-ls/"
 return {
     cmd = { "gitlab-ci-ls" },
     filetypes = { "yaml.gitlab" },
-    root_markers = { ".git", ".gitlab" },
+    root_markers = { ".git" },
     init_options = {
         cache_path = cache_dir,
         log_path = cache_dir .. "/log/gitlab-ci-ls.log",
