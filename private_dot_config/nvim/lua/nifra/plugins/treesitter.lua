6be3fff547f0e26cd5f427689d7f21059ffc4b21
Nicolas MAIGNAN <nicolas.maignan@univ-lorraine.fr>
2026-02-25 22:39:17 +0100

    tree-sitter is horseshit (for now)
    I don't know why, but in facts it was still the "master" branch ... so
    yeah, at least now the config is better (like with the main to have just
    opts) but still ... fucking plugin, it will stay like that for months, I
    do not have the energy right now to search how to use it properly (like
    with a clean config, I imagine with ftplugins ...)

--- private_dot_config/nvim/lua/nifra/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/nifra/plugins/treesitter.lua
@@ -1,11 +1,25 @@
--- NOTE: Highlight, edit, and navigate code
+-- NOTE: tree-sitter is horse shit, at least in terms of configuration in nvim
+
+local parser_configs = require("nvim-treesitter.parsers").get_parser_configs()
+
+parser_configs.kitty = {
+    install_info = {
+        url = "https://github.com/OXY2DEV/tree-sitter-kitty",
+        files = { "src/parser.c" },
+        branch = "main",
+    },
+}
+
 return {
     {
         "nvim-treesitter/nvim-treesitter",
         lazy = false,
         build = ":TSUpdate",
+        branch = "master",
         main = "nvim-treesitter.configs",
         opts = {
+            auto_install = false,
+            ignore_install = { "latex" },
             ensure_installed = {
                 "bash",
                 "bibtex",
@@ -17,8 +31,7 @@ return {
                 "java",
                 "javadoc",
                 "json",
-                -- "kitty",
-                -- "latex",
+                "kitty",
                 "lua",
                 "luadoc",
                 "markdown",
@@ -35,6 +48,8 @@ return {
                 "zathurarc",
                 -- "zsh",
             },
+            highlight = { enable = true },
+            indent = { enable = true },
         },
     },
 }
