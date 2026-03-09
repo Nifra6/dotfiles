7332aa1898cf85edc4b3f6669d5144ded47dda7b
Nicolas MAIGNAN <nicolas.maignan@univ-lorraine.fr>
2026-03-09 16:37:53 +0100

    :fire: Simplify conform.nvim config

--- private_dot_config/nvim/lua/nifra/plugins/format.lua
+++ private_dot_config/nvim/lua/nifra/plugins/format.lua
@@ -8,14 +8,13 @@ return {
             {
                 "<leader>cf",
                 function()
-                    require("conform").format({ async = true, lsp_fallback = true })
+                    require("conform").format()
                 end,
                 desc = "[F]ormat buffer",
             },
         },
         opts = {
-            notify_on_error = true,
-            format_on_save = { timeout_ms = 500, lsp_fallback = true },
+            format_on_save = { lsp_format = "fallback", timeout_ms = 500 },
             formatters_by_ft = {
                 bash = { "shfmt" },
                 bib = { "bibtex-tidy" },
