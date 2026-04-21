ad2b073dd0a392f438a1a56400a82d2e5fc9a4f6
Nicolas MAIGNAN <nicolas.maignan@univ-lorraine.fr>
2026-04-21 15:49:54 +0200

    :lipstick: Update kitty auto tab titles

---
+++ private_dot_config/zsh/kitty_title.zsh
@@ -0,0 +1,26 @@
+autoload -Uz add-zsh-hook
+
+_kitty_precmd() {
+    local parent=${PWD%/*}
+    local dir=${PWD##*/}
+    local short_path="${parent##*/}/$dir"
+    print -Pn "\e]0; $short_path\a"
+}
+
+_kitty_preexec() {
+    local cmd=$1
+    local parent=${PWD%/*}
+    local dir=${PWD##*/}
+    local short_path="${parent##*/}/$dir"
+    local icon
+    case $cmd[(w)1] in
+        v|vim|nvim)  icon=" " ;;
+        python*)     icon="󰌠 " ;;
+        lg|lazygit)  icon="󰊢 " ;;
+        *)           icon=" " ;;
+    esac
+    print -Pn "\e]0;$icon $short_path\a"
+}
+
+add-zsh-hook precmd _kitty_precmd
+add-zsh-hook preexec _kitty_preexec
