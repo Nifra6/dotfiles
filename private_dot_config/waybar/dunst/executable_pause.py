64999ba9e9bf1c785d75aba6568ee5309303b5d0
Nicolas MAIGNAN <nicolas.maignan@univ-lorraine.fr>
2026-09-12 10:05:45 +0200

    :wrench: Forgot to add python dunst script

---
+++ private_dot_config/waybar/dunst/executable_pause.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""Vérifie l'état de pause de dunst et écrit un statut JSON pour waybar."""
+
+import json
+import subprocess
+import sys
+
+
+def main() -> None:
+    """Affiche le statut actuel de dunst."""
+    try:
+        output = subprocess.check_output(
+            ["dunstctl", "is-paused"],  # noqa: S607
+            universal_newlines=True,
+        )
+
+    except subprocess.CalledProcessError:
+        return
+
+    match output:
+        case "true\n":
+            output_text = "Notifications en pause."
+            output_alt = "paused"
+        case "false\n":
+            output_text = "Notifications visibles."
+            output_alt = "running"
+        case _:
+            msg = f"Problème avec dunst, on obtient :'{output}'."
+            raise RuntimeError(msg)
+
+    output = {
+        "text": output_text,
+        "alt": output_alt,
+        "tooltip": output_text,
+        "class": "info",
+    }
+
+    sys.stdout.write(json.dumps(output) + "\n")
+    sys.stdout.flush()
+
+
+if __name__ == "__main__":
+    main()
